bcfa59799de8b1cc0a9f83d850f3637559f6fc15
Jonas Mahn <mahn@fias.uni-frankfurt.de>
2019-05-22 20:37:33 +0200

    New Level

--- experiment.py
+++ experiment.py
@@ -45,6 +45,7 @@ def runExp(*args, **kwargs):
                 a = np.random.randint(0, 5)
             else:
                 a = np.argmax(pacModel.predict(s))
+                print('pred action', a)
             #act here
             if a == 0:
                 action = env.player.ActionSpace.IDLE
@@ -63,7 +64,7 @@ def runExp(*args, **kwargs):
                 currentReward += reward['pacman']
             obs, reward, done, info = env.render()
             currentReward += reward["pacman"]
-            print(currentReward)
+            #print(currentReward)
             r = currentReward
             obspac = np.array(obs["pacman"])
             new_s = np.reshape(obspac, (1,obspac.shape[0],obspac.shape[1],1))
