991526b3883484436dda645fc099561e407cf1d4
Julius Taylor <taylor@fias.science>
2019-05-26 14:04:13 +0200

    Add replaybuffer.py

--- example.py
+++ example.py
@@ -1,20 +1,17 @@
 from envs import mazewandererenv
+from replaybuffer import ReplayBuffer
 from matplotlib import pyplot as plt
 
 env = mazewandererenv.Env()
+buf = ReplayBuffer(max_buffer_size = 3)
 
 i = 0
 while 1:
-    i += 1
     # render the env, get observations and rewards
     obs, reward, done, info = env.render()
-
-    # display what MazeWanderer sees
-    print(type(obs["ghosts"][0]))
-    plt.show()
     
     # pacman und ghosts have  ActionSpace = IDLE, UP, DOWN, LEFT, RIGHT
-    # do some RL stuff
+    # do some RL stuff - choose actions
     if i % 2 == 0:
         env.player.action = env.player.ActionSpace.DOWN
     else:
@@ -22,5 +19,17 @@ while 1:
     env.ghost.action = env.ghost.ActionSpace.DOWN
     env.ghost2.action = env.ghost2.ActionSpace.UP
     env.ghost3.action = env.ghost3.ActionSpace.IDLE
+
+    # put observations into replay buffer
+    # momentan nur die action vom player
+    buf.append(obs["pacman"], reward, env.player.action)
+    if i == 3:
+        break
+
+    i += 1
     if done:
         env.reset()
+
+buf.shuffle()
+obs, reward, action = buf.next_batch(2)
+print(obs.shape, reward.shape, action)
--- replaybuffer.py
+++ replaybuffer.py
@@ -1,13 +1,64 @@
-from envs import mazewandererenv
+import numpy as np
+
+
 class ReplayBuffer:
-    def __init__(self, env: mazewandererenv.Env):
-        self.env = env
+    def __init__(self, max_buffer_size = 500):
+        self.action_buffer = None
+        self.reward_buffer = None
+        self.obs_buffer = None
+        self.max_buffer_size = max_buffer_size
+        self.current_buffer_size = 0
+        self.first = True
+        self.read_idx = 0
+        self.write_idx = 0
     
+    def append(self, observation, reward, action):
+        if self.current_buffer_size < self.max_buffer_size:
+            if self.first:
+                self.obs_buffer = np.expand_dims(observation, axis=0)
+                self.reward_buffer = reward
+                self.action_buffer = action
+                self.first = False
+            else:
+                observation = np.expand_dims(observation, axis=0)
+                self.obs_buffer = np.concatenate((self.obs_buffer, observation), axis=0)
+                self.reward_buffer = np.append(self.reward_buffer, reward)
+                self.action_buffer = np.append(self.action_buffer, action)
+            self.current_buffer_size += 1
+        else:
+            self.obs_buffer[self.write_idx] = observation
+            self.reward_buffer[self.write_idx] = reward
+            self.action_buffer[self.write_idx] = action
+            self.write_idx += 1
+
     def next_batch(self, batch_size):
-        pass
-    
+        if self.read_idx + batch_size <= self.max_buffer_size:
+            obs = self.obs_buffer[self.read_idx:self.read_idx+batch_size]
+            action = self.action_buffer[self.read_idx:self.read_idx+batch_size]
+            reward = self.reward_buffer[self.read_idx:self.read_idx+batch_size]
+            self.read_idx += batch_size
+
+            return obs, reward, action
+        else:
+            tail_indices = batch_size - (self.max_buffer_size - self.read_idx)
+            top_indices = batch_size - tail_indices
+            tail_range = range(self.read_idx + tail_indices)
+            top_range = range(0, top_indices[-1])
+            tail_data = np.take(self.obs_buffer, tail_range, axis=0)
+            top_data = np.take(self.obs_buffer, top_range, axis=0)
+            self.read_idx = top_indices
+            action = np.concatenate(self.action_buffer[tail_range],self.action_buffer[top_range])
+            reward = np.concatenate(self.reward_buffer[tail_range],self.reward_buffer[top_range])
+
+            return np.stack(tail_data, top_data), reward, action
+
     def shuffle(self):
-        pass
-    
-    
-        
+        perm = np.random.permutation(self.current_buffer_size)
+        self.obs_buffer = self.obs_buffer[perm]
+        self.reward_buffer = self.reward_buffer[perm]
+        self.action_buffer = self.action_buffer[perm]
+
+    def __repr__(self):
+        return f"Replay buffer: \n\t \
+            buffer size: \t{self.max_buffer_size}\n\t\
+            current index: \t{self.read_idx}\n\t\ "
