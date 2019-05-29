bd961419250014dfe5c1067eb45167106e18f258
Jonas Mahn <mahn@fias.uni-frankfurt.de>
2019-05-29 14:49:47 +0200

    ToDo Replay Buffer in comments

--- envs/replaybuffer.py
+++ envs/replaybuffer.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+# ToDo & Bugs:
+# - Extra dimension appears when using next_batch at end of buffer
+# - Buffer tuple should be (state, newState, action, reward)
+# - 
 
 class ReplayBuffer:
     def __init__(self, max_buffer_size = 500):
@@ -11,6 +15,7 @@ class ReplayBuffer:
         self.first = True
         self.read_idx = 0
         self.write_idx = 0
+        # ToDo: Allocate full size of buffer at start
     
     def append(self, observation, reward, action):
         if self.current_buffer_size < self.max_buffer_size:
@@ -32,13 +37,14 @@ class ReplayBuffer:
             self.write_idx = self.write_idx % (self.max_buffer_size + 1)
 
     def next_batch(self, batch_size):
+        done = True # ToDo: true only if one round through buffer completed
         if self.read_idx + batch_size <= self.max_buffer_size:
             obs = self.obs_buffer[self.read_idx:self.read_idx+batch_size]
             action = self.action_buffer[self.read_idx:self.read_idx+batch_size]
             reward = self.reward_buffer[self.read_idx:self.read_idx+batch_size]
             self.read_idx += batch_size
 
-            return obs, reward, action
+            return obs, reward, action, done
         else:
             tail_indices = batch_size - (self.max_buffer_size - self.read_idx)
             top_indices = batch_size - tail_indices
@@ -48,13 +54,17 @@ class ReplayBuffer:
             reward = np.concatenate((self.reward_buffer[self.read_idx:],self.reward_buffer[:top_indices]))
             self.read_idx = top_indices
 
-            return np.stack((tail_data, top_data)), reward, action
+            return np.stack((tail_data, top_data)), reward, action, done
 
     def shuffle(self):
         perm = np.random.permutation(self.current_buffer_size)
         self.obs_buffer = self.obs_buffer[perm]
         self.reward_buffer = self.reward_buffer[perm]
         self.action_buffer = self.action_buffer[perm]
+
+    def resetReadIndex(self):
+        # ToDo: implement
+        pass
 
     def __repr__(self):
         return f"Replay buffer: \n\t \
