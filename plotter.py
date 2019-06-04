3e4d43e9cd69e8827f7cac2928f8c58f3cd17d60
Jonas Mahn <mahn@fias.uni-frankfurt.de>
2019-06-04 17:17:04 +0200

    Removed useless plot

--- plotter.py
+++ plotter.py
@@ -3,16 +3,17 @@ from matplotlib import pyplot as plt
 def pacmanAgentPerf(conf, stepsPerGame, rewardPerGame):
     x = list(range(1,len(stepsPerGame)+1))
     
+    fig = plt.figure()
     fig, ax1 = plt.subplots()
     
     ax1.plot(x, rewardPerGame)
     ax1.set_ylabel('Reward')
     ax1.set_xlabel("Game")
 
-    ax2 = ax1.twinx()
-    ax2.plot(x, stepsPerGame, 'orangered')
-    ax2.set_ylabel('Steps', color='orangered')
-    ax2.tick_params(colors='orangered')
+    #ax2 = ax1.twinx()
+    #ax2.plot(x, stepsPerGame, 'orangered')
+    #ax2.set_ylabel('Steps', color='orangered')
+    #ax2.tick_params(colors='orangered')
     
     plt.title("Performance: Pacman Agent")
     plt.xlim((1,len(stepsPerGame)))
