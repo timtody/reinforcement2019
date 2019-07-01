49a295a1b3e62655789f9b7830cf5b0920a62678
Jonas Mahn <mahn@fias.uni-frankfurt.de>
2019-07-01 17:45:57 +0200

    Optimization + benchmark

--- naiveShortestPath.py
+++ naiveShortestPath.py
@@ -1,6 +1,8 @@
 import numpy as np
 import math
 
+from time import time
+
 from envs.level import Level
 
 # Grid Level Int Codes
@@ -45,14 +47,12 @@ def convertLevelFileToGrid(level, printLevel=False):
 def shortestPathDist(levelGrid, start, target):
     levelGrid[start] = -5
     levelGrid[target] = -4
-    print(levelGrid)
     if start != target:
         return explore(levelGrid, start, target)
     else:
         return 0, levelGrid
 
-def explore(levelGrid, currentPos, target, currentSteps = 1, excludeDirection = (None, None)):
-    #print(currentSteps)
+def explore(levelGrid, currentPos, target, currentSteps = 1):
     minSteps = math.inf
     targetFound = False
     for delta in [(+1,0),(0,+1),(0,-1),(-1,0)]:
@@ -63,42 +63,46 @@ def explore(levelGrid, currentPos, target, currentSteps = 1, excludeDirection = 
             if target[0] == expX and target[1] == expY and minSteps > currentSteps:
                 minSteps = currentSteps
                 targetFound = True
-                print('Yeah', minSteps)
+                #print('Yeah', minSteps)
         except IndexError:
             print('Well, shit')
     
     if not targetFound:
         levelGrid[currentPos] = currentSteps -1
         for delta in [(+1,0),(0,+1),(0,-1),(-1,0)]:
-            if delta != excludeDirection:
-                expX = currentPos[0] + delta[0]
-                expY = currentPos[1] + delta[1]
-                
-                if (expX >= 0) and (expY >= 0):
-                    try:
-                        valueOnNewPos = levelGrid[expX, expY]
-                        if valueOnNewPos != -1 and (valueOnNewPos > currentSteps or valueOnNewPos < 0): # If Pos is not a Wall
-                            steps, levelGrid = explore(levelGrid,
-                                               (expX, expY),
-                                               target,
-                                               currentSteps=(currentSteps + 1))
-                            if steps < minSteps:
-                                minSteps = steps
-                    except IndexError:
-                        "Wohoo"
+            expX = currentPos[0] + delta[0]
+            expY = currentPos[1] + delta[1]
+            
+            if (expX >= 0) and (expY >= 0):
+                try:
+                    valueOnNewPos = levelGrid[expX, expY]
+                    if valueOnNewPos != -1 and (valueOnNewPos > currentSteps or valueOnNewPos < 0): # If Pos is not a Wall
+                        steps, levelGrid = explore(levelGrid,
+                                           (expX, expY),
+                                           target,
+                                           currentSteps=(currentSteps + 1))
+                        if steps < minSteps:
+                            minSteps = steps
+                except IndexError:
+                    "Wohoo"
     return minSteps, levelGrid
 
                 
 
+reps = 100
+sumDuration = 0
+for i in range(reps):
+    level = Level("FullSingle")
+    levelGrid = convertLevelFileToGrid(level)
 
-
-
-level = Level("FullSingle")
-levelGrid = convertLevelFileToGrid(level, False)
-
-lenPath, levelGrid = shortestPathDist(levelGrid, (2,2), (8,13))
+    startTime = time()
+    lenPath, levelGrid = shortestPathDist(levelGrid, (2,2), (8,13))
+    duration = time() - startTime
+    sumDuration += duration
+avgDuration = duration / reps
 
 print('Result:')
+print('time', duration)
 print(levelGrid)
 print(lenPath)
 
