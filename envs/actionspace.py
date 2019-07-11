501697704589ec04b248bc8638f4747ad7460e40
Julius Taylor <j.tay@hotmail.de>
2019-07-11 10:16:18 +0200

    Add get_dir method to ActionSpace
    for use in the get_legal_action calculation

--- envs/actionspace.py
+++ envs/actionspace.py
@@ -6,3 +6,15 @@ class ActionSpace(Enum):
     LEFT = 2
     RIGHT = 3
     IDLE = 4
+
+    def get_dir(self):
+        if self.value == 0:
+            return (0, -1)
+        if self.value == 1:
+            return (0, 1)
+        if self.value == 2:
+            return (-1, 0)
+        if self.value == 3:
+            return (1, 0)
+        if self.value == 4:
+            return (0, 0)
