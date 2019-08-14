82e61e2a321b353e90cdd05fc17b9a50ccfa2b23
Julius Taylor <j.tay@hotmail.de>
2019-08-14 17:13:43 +0200

    Add smooth plotter wih optimum lines

--- smooth_plot.py
+++ smooth_plot.py
@@ -12,7 +12,7 @@ color_schemes = {
     "orange": {"light": "#facfa5", "dark": "#ff560e"},
     "blue" : {"light": "#88aab5", "dark": "#1c4957"},
 }
-
+handles = ["a)", "b)", "c)", "d)", "e)", "f)"]
 
 def rolling_average(data, window_size=999):
     # window size can only be odd
@@ -35,50 +35,61 @@ def rolling_average(data, window_size=999):
         data[i] = np.mean(data_in[i:i+window_size])
     return data
 
+def plot(log, ax, name, color, handle, showylabel):
+    x = list(range(1,len(log.pacmanReward[:global_cutoff])+1))
+    y = log.pacmanReward[:global_cutoff]
+
+    # "get" (cheat) optimum
+    y_max = np.max(y)
+    opt = np.ones(len(x)) * y_max
+
+    y_smooth = rolling_average(y, 333)
+    ax.scatter(x[::skip], y[::skip], c=color_schemes[color]["light"],s=scatter_size)
+    ax.plot(x[::skip], y_smooth[::skip], c=color_schemes[color]["dark"], label=handle)
+    ax.plot(opt, linestyle="--", color="gray")
+    legend = ax.legend(loc='upper left', handlelength=0)
+    if showylabel: ax.set(ylabel="Total reward per game")
+
+
+# cli parsing
 parser = argparse.ArgumentParser()
 parser.add_argument('path')
 parser.add_argument('--color', default="orange")
 args = parser.parse_args()
-path = args.path
 color = args.color
+path = args.path
 
-fullPathToHistoryPickle = path
-logs = lg.Logger(loadPath=fullPathToHistoryPickle)
+files = [os.path.abspath(path + '\\' + f) for f in os.listdir(path)]
+file_names_only = os.listdir(path)
+logs = []
+# we list all files in the spcified path - please put only .pickle files there
+for f in files:
+    logs.append(lg.Logger(loadPath=f))
 
-# plotting
-x = list(range(1,len(logs.pacmanReward)+1))
-y = logs.pacmanReward
-y_smooth = rolling_average(y, 333)
+# we create a horizonzal plot for each file
+nrows = len(logs)
 
 import matplotlib
 from matplotlib.backends.backend_pgf import FigureCanvasPgf
 matplotlib.backend_bases.register_backend('pdf', FigureCanvasPgf)
-
 sns.set(context="paper")
 
 # shared x and y labels
-fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True, sharey=True)
+fig, axes = plt.subplots(nrows=nrows, ncols=1, sharex=True, sharey=True)
 
 plt.xlabel('Number of games played')
 plt.tight_layout()
 scatter_size = 0.8
-skip = 32
-axes[0].scatter(x[::skip], y[::skip], c=color_schemes["orange"]["light"],s=scatter_size)
-axes[0].plot(x[::skip], y_smooth[::skip], c=color_schemes["orange"]["dark"], label="Model A")
-#legend.get_frame().set_facecolor('C0')
-legend = axes[0].legend(loc='upper left')
+skip = 40 # adjust depending on granularity and available memory - this easily overflows when generating a pgf
+global_cutoff = 50000
 
 
-axes[1].scatter(x[::skip], y[::skip], c=color_schemes["green"]["light"], s=scatter_size)
-axes[1].plot(x[::skip], y_smooth[::skip], c=color_schemes["green"]["dark"], label="Model B")
-axes[1].set(ylabel="Total reward per game")
-legend = axes[1].legend(loc='upper left')
+axes = [axes] if nrows == 1 else axes # oof (thanks python)
 
-
-axes[2].scatter(x[::skip], y[::skip], c=color_schemes["blue"]["light"],s=scatter_size)
-axes[2].plot(x[::skip], y_smooth[::skip], c=color_schemes["blue"]["dark"], label="Model C")
-legend = axes[2].legend(loc='upper left')
-
+# now do the actual plotting
+for i, log in enumerate(logs):
+    showylabel = True if i == len(logs) // 2 else False
+    plot(log, axes[i], file_names_only[i], color, handles[i], showylabel)
 
 plt.savefig("test.pdf")
 plt.savefig("test.pgf")
